0d386f3cdb1ff76b7e06ae2f5a3a95dad69e5ce3
Gabriel Montoya <gabi.montoya@gmail.com>
2024-09-05 15:07:50 -0300

    Created custom hook for use books Context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,12 @@
 import BookShow from "./BookShow";
-import BooksContext from "../context/books";
-import {useContext} from "react";
+import useBooksContext from "../hooks/use-books-context";
+
+
+
 
 function BookList() {
 
-    const {books} = useContext(BooksContext);
+    const {books} = useBooksContext();
 
     const renderedBooks = books.map((book) => <BookShow key={book.id} book={book} />)
 
