21a25e2fd11adc8ae7f2825db7692034674e802e
Gabriel Montoya <gabi.montoya@gmail.com>
2024-09-05 11:51:50 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import axios from "axios";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [books, setBooks] = useState([]);
+
+    const fetchBooks = async () => {
+        try{
+            const response = await axios.get('http://localhost:3001/books');
+            setBooks(response.data);
+        } catch (err){
+            console.error(`Error while trying to create a new book, ${err}`)
+        }
+    };
+
+    useEffect(() => {
+         fetchBooks().then();
+    }, []);
+
+    const deleteBookById = (id) => {
+
+        axios.delete(`http://localhost:3001/books/${id}`)
+            .then((response) => {
+                setBooks(books.filter((book) => book.id !== id));
+            })
+            .catch((err) => {console.error(`Error while trying to delete book with id ${id}, ${err}`)})
+
+
+    }
+
+    const createBook =  (title) => {
+        axios.post('http://localhost:3001/books', {title})
+            .then((response) => {
+                const {id, title} = response.data
+                setBooks([...books,{id, title}]);
+            })
+            .catch((err) => {console.error(`Error while trying to create a new book, ${err}`)})
+
+    };
+
+    const editBookById = (id, newTitle) => {
+
+        axios.put(`http://localhost:3001/books/${id}`, {title: newTitle})
+            .then((response) => {
+                setBooks(books.map((book) => {
+                    if(book.id === response.data.id) {
+                        return {
+                            ...book,
+                            ...response.data
+                        }
+                    }
+                    return book;
+                }));
+
+            })
+            .catch((err) => {console.error(`Error while trying to edit book with id ${id}, ${err}`)})
+    }
+
+    return (
+        <div className={"app"}>
+            <h1>Reading list</h1>
+            <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
+            <BookCreate onCreate={createBook}/>
+        </div>
+    );
 }
 
 export default App;
